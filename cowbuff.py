ff1b2b4795302db604a04dabad8191335783caf6
NJWasTaken <114809171+NJWasTaken@users.noreply.github.com>
2023-10-14 09:33:01 +0530

    N-letter guess

--- cowbuff.py
+++ cowbuff.py
@@ -1,8 +1,9 @@
 import tkinter as tk
-from tkinter import messagebox
+from tkinter import messagebox,simpledialog
 import random
 import sys
 
+n=int
 class HomePage:
     def __init__(self, master):
         self.master = master
@@ -25,7 +26,82 @@ class HomePage:
         self.bulls_and_cows_game.mainloop()
 
     def words_button_click(self):
-        messagebox.showinfo("Words Button Clicked", "You clicked the Words button.")
+        global n
+        root.destroy()
+        n = simpledialog.askinteger("Enter n", "Enter the number of letters in the word:")
+        if n is not None:
+            self.wordle = tk.Tk()
+            self.wordle.title("Wordle with Bulls and Cows")
+            game = Wordle(self.wordle)
+            self.wordle.mainloop()
+class Wordle:
+    def __init__(self, master):
+        global n
+        self.master = master
+        self.secret_code = self.generate_secret_code()
+        self.num_guesses = 0
+        self.guess_history = []
+
+        self.bg_image = tk.PhotoImage(file="bg.png")
+        self.bg_canvas = tk.Canvas(master, width=1250, height=800)
+        self.bg_canvas.create_image(0, 0, anchor=tk.NW, image=self.bg_image)
+        self.bg_canvas.pack()
+
+        self.label = tk.Label(self.bg_canvas, text=f"Enter your guess ({n}-letter word):", font=("Arial", 16), bg="white")
+        self.label.place(relx=0.5, rely=0.1, anchor=tk.CENTER)
+
+        self.guess_entry = tk.Entry(self.bg_canvas, font=("Arial", 16))
+        self.guess_entry.place(relx=0.5, rely=0.2, anchor=tk.CENTER)
+
+        self.submit_button = tk.Button(self.bg_canvas, text="Submit Guess", font=("Arial", 16), command=self.check_guess)
+        self.submit_button.place(relx=0.5, rely=0.3, anchor=tk.CENTER)
+
+        self.result_label = tk.Label(self.bg_canvas, text="", font=("Arial", 16), bg="white")
+        self.result_label.place(relx=0.5, rely=0.4, anchor=tk.CENTER)
+
+        self.guess_history_text = tk.Text(self.bg_canvas, height=10, width=30, font=("Arial", 14))
+        self.guess_history_text.place(relx=0.5, rely=0.5, anchor=tk.CENTER)
+
+    def generate_secret_code(self):
+        global n
+        return ''.join(random.sample('qwertyuiopasdfghjklzxcvbnm', n))
+
+    def check_guess(self):
+        global n
+        guess = self.guess_entry.get()
+        if len(guess) != n:
+            messagebox.showerror("Error", f"Please enter a valid {n}-letter word.")
+            return
+
+        self.num_guesses += 1
+        bulls, cows = self.calculate_bulls_and_cows(guess)
+        self.guess_history.append((guess, bulls, cows))
+
+        if bulls == n:
+            messagebox.showinfo("Congratulations", f"You guessed the secret code {self.secret_code} in {self.num_guesses} tries!")
+            self.master.destroy()
+        else:
+            self.result_label.config(text=f"Bulls: {bulls}, Cows: {cows}")
+            self.update_guess_history()
+
+    def calculate_bulls_and_cows(self, guess):
+        global n
+        bulls = sum(1 for i in range(n) if guess[i] == self.secret_code[i])
+        cows = sum(1 for digit in guess if digit in self.secret_code) - bulls
+        return bulls, cows
+
+    def update_guess_history(self):
+        self.guess_history_text.delete('1.0', tk.END)
+        for guess, bulls, cows in self.guess_history:
+            self.guess_history_text.insert(tk.END, f"Guess: {guess}, Bulls: {bulls}, Cows: {cows}\n")
+
+if __name__ == '__main__':
+    root = tk.Tk()
+    root.title("Home Page")
+    home_page = HomePage(root)
+    root.mainloop()
+
+
 
 class BullsAndCowsGame:
     def __init__(self, master):
